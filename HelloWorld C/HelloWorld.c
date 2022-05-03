d325b7e73c269b5292c598792cd1a67d4d3b071d
Mason Knight <92546628+masonknight22@users.noreply.github.com>
2022-05-03 00:02:43 -0400

    Add files via upload

---
+++ HelloWorld C/HelloWorld.c
@@ -0,0 +1,19 @@
+/**
+ * HelloWorld
+ *
+ * A program that states "Hello World!"
+ *
+ * Purdue University - CS 240
+ *
+ * Author: Mason Knight
+ *
+ * Version: January 29 2021
+ *
+ *
+ */
+
+#include <stdio.h>
+int main() {
+   printf("Hello, World!");
+   return 0;
+}
